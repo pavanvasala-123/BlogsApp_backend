ce21e271efbc7890d0e92122fcb319ba365ccf82
pavanvasala-123 <pavanvasalaa@gmail.com>
2024-07-08 16:44:43 +0530

    implemented update controller

--- Routes/blog.js
+++ Routes/blog.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createBlog,getAllBlogs , getUserBlogs ,deleteBlog} = require('../Controllers/blog')
+const {createBlog,getAllBlogs , getUserBlogs ,deleteBlog,findBlogById,updateBlogById} = require('../Controllers/blog')
 const verifyToken = require('../Middleware/AuthController')
 const app = require('../server')
 
 router.post('/createblog',verifyToken, createBlog).get("/", getAllBlogs).get('/user',verifyToken,getUserBlogs).delete('/:id' , deleteBlog)
+router.get('/blog/:id',verifyToken, findBlogById);
+router.put('/blog/:id',verifyToken, updateBlogById);
 
 module.exports = router;
